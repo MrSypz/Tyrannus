06ae636be1b8bf718571a2b7901a4d35dfa1823a
chai <chainot@hotmail.co.th>
2024-07-07 16:20:28 +0700

    0.1.2 init getNullable method

--- src/main/java/sypztep/tyrannus/Tyrannus.java
+++ src/main/java/sypztep/tyrannus/Tyrannus.java
@@ -1,8 +1,12 @@
 package sypztep.tyrannus;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Tyrannus implements ModInitializer {
+    public static final String MODID = "tyrannus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     @Override
     public void onInitialize() {
 
