5044854c999eac2c4f04485dcc6e26052ccec27a
chain <chainot@hotmail.co.th>
2025-04-03 22:08:39 +0700

    bruh

--- src/main/java/sypztep/tyrannus/TyrannusClient.java
+++ src/main/java/sypztep/tyrannus/TyrannusClient.java
@@ -1,11 +1,9 @@
 package sypztep.tyrannus;
 
 import net.fabricmc.api.ClientModInitializer;
-import sypztep.tyrannus.client.widget.TabWidgetRegistry;
 
 public class TyrannusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        TabWidgetRegistry.clearTabs();
     }
 }
